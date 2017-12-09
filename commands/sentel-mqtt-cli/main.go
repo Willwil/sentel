899033ea1f7f66ee6d9f9c57c9782b4c5a4d3abe
ssddn <songan@gmail.com>
2017-12-09 18:47:51 +0800

    perfect mqtt client tools

--- commands/sentel-mqtt-cli/main.go
+++ commands/sentel-mqtt-cli/main.go
@@ -19,34 +19,47 @@ func main() {
 	//create a ClientOptions struct setting the broker address, clientid, turn
 	//off trace output and set the default message handler
 	opts := MQTT.NewClientOptions().AddBroker("tcp://localhost:1883")
-	opts.SetClientID("go-simple")
+	opts.SetClientID("mqtt-client|securemode=3,signmethod=hmacsha1,timestamp=12345")
+	opts.SetUsername("device1&product2")
+	opts.SetPassword("123456")
 	opts.SetDefaultPublishHandler(f)
+	opts.SetCleanSession(true)
 
 	//create and start a client using the above ClientOptions
 	c := MQTT.NewClient(opts)
 	if token := c.Connect(); token.Wait() && token.Error() != nil {
 		panic(token.Error())
 	}
+	fmt.Println("Mqtt Connect success")
 
 	//subscribe to the topic /go-mqtt/sample and request messages to be delivered
 	//at a maximum qos of zero, wait for the receipt to confirm the subscription
-	if token := c.Subscribe("go-mqtt/sample", 0, nil); token.Wait() && token.Error() != nil {
+	if token := c.Subscribe("device1/product1/data", 0, nil); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
+	fmt.Println("Mqtt SUBSCRIBE success")
+	time.Sleep(3 * time.Second)
 
 	//Publish 5 messages to /go-mqtt/sample at qos 1 and wait for the receipt
 	//from the server after sending each message
 	for i := 0; i < 5; i++ {
 		text := fmt.Sprintf("this is msg #%d!", i)
-		token := c.Publish("go-mqtt/sample", 0, false, text)
+		token := c.Publish("device1/product1/data", 0, false, text)
 		token.Wait()
+		if token.Error() != nil {
+			fmt.Println("Failed to PUBLISH message to topic")
+		} else {
+			fmt.Println("Mqtt PUBLISH success")
+		}
+
+		time.Sleep(1 * time.Second)
 	}
 
 	time.Sleep(3 * time.Second)
 
 	//unsubscribe from /go-mqtt/sample
-	if token := c.Unsubscribe("go-mqtt/sample"); token.Wait() && token.Error() != nil {
+	if token := c.Unsubscribe("device1/product1/data"); token.Wait() && token.Error() != nil {
 		fmt.Println(token.Error())
 		os.Exit(1)
 	}
