a75d8c6a38555dd9d3a075e79feb737ea59db954
cloustone <jenson.zuo@qq.com>
2018-01-06 16:16:40 +0800

    add testcase for keystone in keystone example folder

--- keystone/example/main.go
+++ keystone/example/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 
 	"github.com/cloustone/sentel/keystone/client"
-	l2 "github.com/cloustone/sentel/keystone/l2os"
 	"github.com/cloustone/sentel/keystone/ram"
 	"github.com/cloustone/sentel/pkg/config"
 	"github.com/golang/glog"
@@ -13,19 +12,22 @@ import (
 func main() {
 	flag.Parse()
 	c := config.New()
-	client.Initialize(c)
 	c.AddConfig(map[string]map[string]string{
 		"keystone": {
-			"hosts": "localhost:4146",
+			"hosts": "localhost:4147",
 		},
 	})
-
+	client.Initialize(c)
 	glog.Error(client.CreateAccount("account1"))
 	glog.Error(client.CreateResource("account1", ram.ResourceCreateOption{
-		Name: "product1",
+		Name:       "product1",
+		Attributes: []string{"devices", "rules"},
 	}))
-	glog.Error(client.AddResourceGrantee("product1", "client1", l2.RightRead))
-	glog.Error(client.AccessResource("product1", "client1", l2.ActionWrite))
+	glog.Error(client.AddResourceGrantee("product1", "client1", ram.RightRead))
+	glog.Error(client.AddResourceGrantee("product1/devices", "client1", ram.RightRead))
+	glog.Error(client.AddResourceGrantee("product1/devices", "client1", ram.RightWrite))
+	glog.Error(client.Authorize("product1", "client1", "r"))
+	glog.Error(client.Authorize("product1/devices", "client1", "r"))
 	glog.Error(client.DestroyResource("product1", "account1"))
 	glog.Error(client.DestroyAccount("account1"))
 }
